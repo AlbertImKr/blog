376206de455844252c09a3baeafa1c62305ce2d6
ALbertIM0427 <guang7226961@naver.com>
2024-08-27 16:46:03 +0900

    feat: 회원 가입 기능 구현

---
+++ user/forms.py
@@ -0,0 +1,34 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+
+class UserSignupForm(UserCreationForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
+
+    def __init__(self, *args, **kwargs):
+        super(UserSignupForm, self).__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs.update(
+                {'class': 'form-control', 'Id': 'username',
+                 'placeholder': 'Username', 'required': 'True',
+                 'aria-describedby': 'usernameHelp'})
+        self.fields['email'].widget.attrs.update(
+                {'class': 'form-control', 'id': 'email',
+                 'placeholder': 'Email', 'required': 'True'})
+        self.fields['password1'].widget.attrs.update(
+                {'class': 'form-control', 'id': 'password',
+                 'placeholder': 'Password', 'required': 'True'})
+        self.fields['password2'].widget.attrs.update(
+                {'class': 'form-control', 'id': 'confirm_password',
+                 'placeholder': 'Confirm Password', 'required': 'True'})
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        exists = User.objects.filter(email=email).exists()
+        if exists:
+            raise forms.ValidationError('이메일이 이미 존재합니다.')
+        return email
--- user/views.py
+++ user/views.py
@@ -1,3 +1,20 @@
+from django.shortcuts import redirect
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+from .forms import UserSignupForm
+
+
+class SignupView(View):
+    def get(self, request):
+        form = UserSignupForm()
+        return render(request, 'user/signup.html',
+                      {'form': form})
+
+    def post(self, request):
+        form = UserSignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+        return render(request, 'user/signup.html',
+                      {'error': form.errors, 'form': form})
