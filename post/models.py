2118c8ef4a1f4f5c9c2b36937eb7fb54cf391c38
ALbertIM0427 <guang7226961@naver.com>
2024-08-27 09:47:07 +0900

    feat: Category model 구현 및 Post와 관계 연결

--- post/models.py
+++ post/models.py
@@ -1,4 +1,3 @@
-from django.contrib.auth.models import User
 from django.db import models
 
 
@@ -9,15 +8,24 @@ class Post(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     status = models.BooleanField(default=True)
     tags = models.ManyToManyField('Tag', related_name='posts')
-    author = models.ForeignKey(User, on_delete=models.CASCADE,
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE,
                                related_name='posts')
+    category = models.ForeignKey('Category', on_delete=models.CASCADE,
+                                 related_name='posts')
 
     def __str__(self):
         return self.title
 
 
 class Tag(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
 
     def __str__(self):
         return self.name
