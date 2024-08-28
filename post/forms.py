3d77535c530fc305f305d7b7a15f33a80149b7a7
ALbertIM0427 <guang7226961@naver.com>
2024-08-28 12:16:00 +0900

    refactor: 게시글 작성 페이지 인증 필요 페이지로 변경

--- post/forms.py
+++ post/forms.py
@@ -1,23 +1,33 @@
 from django import forms
 
 from .models import Post
+from .models import Tag
 
 
 class PostCreateForm(forms.ModelForm):
+    tags = forms.CharField(required=False,
+                           widget=
+                           forms.Textarea(
+                                   attrs={'class': 'form-control',
+                                          'rows': '1',
+                                          'placeholder': 'business, finance, health ...'})
+                           )
+
     class Meta:
         model = Post
-        fields = ['title', 'content', 'status', 'tags', 'category']
+        fields = ['title', 'description', 'content', 'status', 'category']
         widgets = {
             'title': forms.TextInput(
                     attrs={'required': True, 'id': 'con-name', 'name': 'name',
                            'type': 'text', 'class': 'form-control',
                            'placeholder': 'Post title'}),
+            'description': forms.Textarea(
+                    attrs={'required': True, 'class': 'form-control',
+                           'rows': '3', 'placeholder': 'Add description'}),
             'content': forms.Textarea(
                     attrs={'class': 'd-none', 'rows': '10',
-                           'placeholder': 'Add content', 'id': 'quill-content'}),
-            'tags': forms.SelectMultiple(
-                    attrs={'class': 'form-control', 'rows': '1',
-                           'placeholder': 'business, finance, health'}),
+                           'placeholder': 'Add content',
+                           'id': 'quill-content'}),
             'category': forms.Select(
                     attrs={'class': 'form-select',
                            'aria-label': 'Default select example'}),
@@ -25,3 +35,23 @@ class PostCreateForm(forms.ModelForm):
                     attrs={'class': 'form-check-input', 'type': 'checkbox',
                            'id': 'postCHeck'}),
         }
+
+    def clean_tags(self):
+        tags_string = self.cleaned_data.get('tags', '')
+        tags = [tag.strip() for tag in tags_string.split(',') if
+                tag.strip()]
+        if len(tags) > 14:
+            raise forms.ValidationError('최대 14개의 태그만 입력할 수 있습니다.')
+        return tags
+
+    def save(self, commit=True):
+        post = super().save(commit=False)
+        post.save()
+        tags_names = self.cleaned_data.get('tags', [])
+        tags = []
+        for tag_name in tags_names:
+            tag, _ = Tag.objects.get_or_create(name=tag_name)
+            tags.append(tag)
+        if tags:
+            post.tags.set(tags)
+        return post
