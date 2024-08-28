194912bf6f5d6d508a771691d9007cd7aadf0748
ALbertIM0427 <guang7226961@naver.com>
2024-08-28 17:41:51 +0900

    feat: 게시글 조회 기능 구현

--- post/urls.py
+++ post/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 
 from .views import HomeView
 from .views import PostCreateView
+from .views import PostDetailView
 
 urlpatterns = [
     path("", HomeView.as_view(), name="home"),
     path("posts/new", PostCreateView.as_view(), name="post_create"),
+    path("posts/<int:pk>", PostDetailView.as_view(), name="post_detail"),
 ]
