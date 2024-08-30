bb1cfb3d610a520e25af1695610ce162ef5d59ee
ALbertIM0427 <guang7226961@naver.com>
2024-08-30 22:29:27 +0900

    feat: contact 구현

--- blog/urls.py
+++ blog/urls.py
@@ -2,8 +2,11 @@ from django.contrib import admin
 from django.urls import include
 from django.urls import path
 
+from .views import ContactView
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("post.urls")),
     path("", include("user.urls")),
+    path("contact/", ContactView.as_view(), name="contact"),
 ]
