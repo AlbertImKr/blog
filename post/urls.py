a321cc1f260550d682bee9bc069968b8ad2c4cec
ALbertIM0427 <guang7226961@naver.com>
2024-08-31 17:16:30 +0900

    refactor: 포스트 삭제 기능 개선

--- post/urls.py
+++ post/urls.py
@@ -1,21 +1,21 @@
 from django.urls import path
 
+from .views import CategoryListView
 from .views import HomeView
 from .views import PostCreateView
+from .views import PostDeleteView
 from .views import PostDetailView
 from .views import PostListFragmentGridView
 from .views import PostListFragmentView
 from .views import PostListView
 from .views import PostUpdateView
-from .views import delete_post
-from .views import CategoryListView
 
 urlpatterns = [
     path("", HomeView.as_view(), name="home"),
     path("posts/new", PostCreateView.as_view(), name="post_create"),
     path("posts/<int:pk>", PostDetailView.as_view(), name="post_detail"),
     path("posts/<int:pk>/edit", PostUpdateView.as_view(), name="post_update"),
-    path("posts/<int:pk>/delete", delete_post, name="post_delete"),
+    path("posts/<int:pk>/delete", PostDeleteView.as_view(), name="post_delete"),
     path("posts/", PostListView.as_view(), name="post_list"),
     path("posts/fragment/", PostListFragmentView.as_view(),
          name="post_list_partial"),
